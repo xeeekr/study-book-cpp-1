1499e1ab9e9971b932c530efc102c12329302c49
xeeekr <xeeekr@gmail.com>
2024-11-05 11:28:29 +0900

    📝 DAY-04 : Ch06

---
+++ ch06/ch06-35.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main() {
+	list<int> lt;
+
+	lt.push_back(10);
+	lt.push_back(20);
+	lt.push_back(30);
+	lt.push_back(10);
+	lt.push_back(40);
+	lt.push_back(50);
+	lt.push_back(10);
+	lt.push_back(10);
+
+
+	list<int>::iterator iter;
+	for (iter = lt.begin() ; iter != lt.end() ; ++iter) {
+		cout << *iter << ' ';
+	}
+	cout << endl;
+
+	lt.remove(10);
+	for (iter = lt.begin() ; iter != lt.end() ; ++iter) {
+		cout << *iter << ' ';
+	}
+	cout << endl;
+
+	return 0;
+}
