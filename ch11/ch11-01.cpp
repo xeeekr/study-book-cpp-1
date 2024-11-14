585178fe2d95a75a34be7dabaff40df7fa15631d
xeeekr <xeeekr@gmail.com>
2024-11-14 13:07:12 +0900

    📝 DAY-08 : ch11

---
+++ ch11/ch11-01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	stack<int> st;
+
+	st.push(10);
+	st.push(20);
+	st.push(30);
+
+	while (!st.empty()) {
+		cout << st.top() << endl;
+		st.pop();
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ ch11/ch11-03.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+#include <deque>
+
+using namespace std;
+
+int main() {
+	priority_queue<int> pq1;
+	pq1.push(40);
+	pq1.push(20);
+	pq1.push(30);
+	pq1.push(50);
+	pq1.push(10);
+
+	cout << "priority_queue[less] : " << endl;
+	while (!pq1.empty()) {
+		cout << pq1.top() << endl;
+		pq1.pop();
+	}
+	cout << "===========" << endl;
+
+	priority_queue<int, deque<int>, greater<int>> pq2;
+	pq2.push(40);
+	pq2.push(20);
+	pq2.push(30);
+	pq2.push(50);
+	pq2.push(10);
+
+	cout << "priority_queue[greater] : " << endl;
+	while (!pq2.empty()) {
+		cout << pq2.top() << endl;
+		pq2.pop();
+	}
+
+	return EXIT_SUCCESS;
+}
